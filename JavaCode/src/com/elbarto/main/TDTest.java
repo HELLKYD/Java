6e3060167fc8a983d34df6e479abe57a90df02f9
HELLKYD <jakobr@posteo.de>
2022-01-31 16:23:51 +0100

    Deleted Sender and Client Created TransferData and TDTest

---
+++ JavaCode/src/com/elbarto/main/TDTest.java
@@ -0,0 +1,15 @@
+package com.elbarto.main;
+
+public class TDTest {
+
+	public static void main(String[] args) {
+
+		String path1 = "/home/jakob/Dokumente/inFile";
+		String path2 = "/home/jakob/Dokumente/outFile";
+
+		TransferData transfer = new TransferData();
+		transfer.transfer(path1, path2);
+
+	}
+
+}
---
+++ JavaCode/src/com/elbarto/main/TransferData.java
@@ -0,0 +1,41 @@
+package com.elbarto.main;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class TransferData {
+
+	public void transfer(String startPath, String endPath) {
+
+		File startFile = new File(startPath);
+		File endFile = new File(endPath);
+
+		InputStream inStream = null;
+		OutputStream outStream = null;
+
+		try {
+			inStream = new FileInputStream(startFile);
+			outStream = new FileOutputStream(endFile);
+
+			int input;
+			while ((input = inStream.read()) != -1) {
+				outStream.write(input);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				inStream.close();
+				outStream.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		System.out.println("Process finished");
+	}
+
+}
