ddc1fd56e5d0e9c5389601a4f75856b000def92c
HELLKYD <jakobr@posteo.de>
2022-03-14 18:56:21 +0100

    Changed Working Pricipe of the lexer

--- JavaCode/src/com/elbarto/lexical/Lexer.java
+++ JavaCode/src/com/elbarto/lexical/Lexer.java
@@ -3,70 +3,83 @@ package com.elbarto.lexical;
 import java.util.ArrayList;
 
 public class Lexer {
+	private static String[] letters;
+	private static String[] numbers = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+	private static ArrayList<IntToken> intTokens = new ArrayList<>();
+	private static int cur_Pos = 0;
 
-	private String sentence;
-	private String[] splitted;
-	private static final String LS_LOL = "LOL";
-	private static final String LS_AND = "and";
-	private static final String LS_LIL = "LIL";
-	private static final String DIGITS = "0123456789";
-
-	public Lexer(String sentence) {
-		this.sentence = sentence;
+	public static void main(String[] args) {
+		String line = "295 + 3.5";
+		Analyze(line);
 	}
 
-	public void analyze() {
-		this.splitted = splitSent(this.sentence);
-		ArrayList<LSToken> tokens = analyzeForToken(splitted);
-		printTokens(tokens);
+	public static void Analyze(String line) {
+		readAndSplit(line);
+		searchForNumberTypes();
+		PrintIntTokens();
+		cur_Pos = 0;
 	}
 
-	private static void printTokens(ArrayList<LSToken> tokens) {
-		for (LSToken cur : tokens) {
-			System.out.println(cur.printType());
+	private static void readAndSplit(String toRead) {
+		String[] chars = new String[toRead.length()];
+		for (int i = 0; i < toRead.length(); i++) {
+			// System.out.println(toRead.substring(i,i+1));
+			chars[i] = toRead.substring(i, i + 1);
 		}
+		letters = chars;
 	}
 
-	private static ArrayList<LSToken> analyzeForToken(String[] input) {
-		ArrayList<LSToken> tokens = new ArrayList<LSToken>();
-		for (String cur : input) {
-			String temp = cur;
-			if (checkForInt(temp)) {
-				LSToken temp_tok = new LSToken(TokenTypes.LS_INT, Integer.parseInt(temp));
-				tokens.add(temp_tok);
-			} else if (checkForFloat(temp)) {
-				LSToken temp_tok = new LSToken(TokenTypes.LS_FLOAT, Float.parseFloat(temp));
-				tokens.add(temp_tok);
+	private static void searchForNumberTypes() {
+		int IntCounter = 0;
+		while (cur_Pos < letters.length) {
+			String cur = letters[cur_Pos];
+			if (cur.equals(" ") || cur.equals("+") || cur.equals(".")) {
+				cur_Pos++;
+				continue;
 			} else {
-				LSToken temp_tok = new LSToken(TokenTypes.LS_INVALID, null);
-				System.exit(0);
+				String not_converted_chars = cur;
+				int index = cur_Pos + 1;
+				while (index < letters.length) {
+					String temp = letters[index];
+					if (temp.equals(" ") || temp.equals("+") || temp.equals(".")) {
+						break;
+					} else {
+						not_converted_chars += temp;
+						index++;
+					}
+				}
+				try {
+					IntCounter++;
+					int tok_value = Integer.parseInt(not_converted_chars);
+					IntToken temp_tok = new IntToken(tok_value);
+					intTokens.add(temp_tok);
+					cur_Pos = index;
+				} catch (NumberFormatException e) {
+					System.out.println("Error: Number too large");
+					System.exit(404);
+				}
 			}
 		}
-		return tokens;
+		System.out.println("Integer: " + IntCounter);
 	}
 
-	private static boolean checkForFloat(String pot_float) {
-		try {
-			float i = Float.parseFloat(pot_float);
-		} catch (NumberFormatException e) {
-			System.out.println("Error");
-			return false;
+	private static void PrintIntTokens() {
+		for (IntToken cur : intTokens) {
+			cur.printToken();
 		}
-		return true;
 	}
 
-	private static boolean checkForInt(String potInt) {
-		try {
-			int i = Integer.parseInt(potInt);
-		} catch (NumberFormatException e) {
-			System.out.println("Error");
-			return false;
+	private static class IntToken {
+		private int value;
+		private String type;
+
+		public IntToken(int value) {
+			this.value = value;
+			this.type = "Int";
 		}
-		return true;
-	}
 
-	private static String[] splitSent(String sentence) {
-		return sentence.split(" ");
+		public void printToken() {
+			System.out.println("Type: " + type + " Value: " + value);
+		}
 	}
-
 }
